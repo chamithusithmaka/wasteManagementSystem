7dbf73234ac03cfa179cac96d1508a7c4e62cbdb
chamithusithmaka <177305347+chamithusithmaka@users.noreply.github.com>
2025-10-15 11:55:08 +0530

    Add routes for BinFullToSchedule and PaymentsPage in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import UserLogin from './UserLogin';
 import UserSignup from './UserSignup';
+import BinFullToSchedule from './TestingPurpose/pages/BinFullToSchedule';
+import PaymentsPage from './pages/PaymentsPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignup />} />
+        <Route path="/bin-full-to-schedule" element={<BinFullToSchedule />} />
+        <Route path="/payments" element={<PaymentsPage />} />
       </Routes>
     </BrowserRouter>
   )
